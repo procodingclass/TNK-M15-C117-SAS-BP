aad180ad2049c0a64baf2dd1ee5d38e1e7d0c032
procodingclass <88050205+procodingclass@users.noreply.github.com>
2023-11-14 14:13:57 +0530

    Add files via upload

---
+++ AA2/main.py
@@ -0,0 +1,48 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+class EmailSenderApp():
+    def __init__(self):
+        super().__init__()
+
+    def send_single_email(self):
+        sender_email = input("Enter Sender Email\n")
+        sender_password = input("Enter Sender Password\n")
+        recipients_email = input(
+            "Enter Comma Seprated Recipient Email\n").strip()
+        subject = input("Enter Email Subject\n")
+        message_body = input("Enter Message\n")
+        recipients_email = recipients_email.split(",")
+
+        for email_id in recipients_email:
+            try:
+                # Fix the host and port number to setup SMTP server
+                smtp_server = smtplib.SMTP("rmtp.ymail.com", 587)
+                # Start the TLS service
+
+                # Login with creadentials
+
+                message = MIMEMultipart()
+                message["From"] = sender_email
+                message["To"] = email_id
+                message["Subject"] = subject
+                message.attach(MIMEText(message_body, "plain"))
+
+                smtp_server.sendmail(
+                    sender_email, email_id, message.as_string())
+                smtp_server.quit()
+                print("Email Sent", "Email sent successfully!")
+
+            except Exception as e:
+                print("Error", f"An error occurred: {str(e)}")
+
+
+def main():
+    app = EmailSenderApp()
+    app.send_single_email()
+
+
+if __name__ == "__main__":
+    main()
